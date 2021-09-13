af04e10b105a2f774e1af978e3fd224d5b9cb6b4
Hyein <ltbl0528@gmail.com>
2021-09-13 13:53:16 +0900

    edit bottom navigation & add floating button

--- app/src/main/java/com/example/wookie/Group/GroupListActivity.java
+++ app/src/main/java/com/example/wookie/Group/GroupListActivity.java
@@ -16,7 +16,7 @@ import com.example.wookie.R;
 public class GroupListActivity extends AppCompatActivity {
 
     private String TAG = "GroupListActivity";
-    private Button enterBtn;
+    private Button enterBtn, addGroupBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,11 +24,20 @@ public class GroupListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_group);
 
         enterBtn = findViewById(R.id.enter_btn);
+        addGroupBtn = findViewById(R.id.add_group_btn);
 
         enterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(GroupListActivity.this, BottomNaviActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        addGroupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GroupListActivity.this, MakeGroupActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/wookie/Group/MakeGroupActivity.java
@@ -0,0 +1,16 @@
+package com.example.wookie.Group;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.wookie.R;
+
+public class MakeGroupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_make);
+    }
+}
