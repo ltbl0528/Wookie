5dc53b0b3e4649aa452a41d25bdec7c4df3b6638
Eugene Kim <ekim9725@naver.com>
2021-09-17 22:52:58 +0900

    Link kakaoMap api & show map in MapActivity, Edit frameLayout size

--- app/src/main/java/com/example/wookie/Map/MapActivity.java
+++ app/src/main/java/com/example/wookie/Map/MapActivity.java
@@ -11,9 +11,14 @@ import androidx.fragment.app.Fragment;
 
 import com.example.wookie.R;
 
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
+
 public class MapActivity extends Fragment {
 
     private View view;
+    private MapView mapView;
     private EditText searchText;
 
     public MapActivity(){
@@ -25,6 +30,31 @@ public class MapActivity extends Fragment {
 
         view = inflater.inflate(R.layout.activity_map, container, false);
 
+        // 카카오맵 띄우기
+        mapView = new MapView(getActivity());
+        ViewGroup mapViewContainer = (ViewGroup)view.findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+        // 좌표 지정
+        MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.45106423452633, 127.12713721912326);
+
+        // 지정된 좌표로 카카오맵 중심점 변경
+        mapView.setMapCenterPoint(MARKER_POINT, true);
+        // 줌 인
+        mapView.zoomIn(true);
+        // 줌 아웃
+        //mapView.zoomOut(true);
+
+        // 마커 표시
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("Default Marker");
+        marker.setTag(0);
+        marker.setMapPoint(MARKER_POINT);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+        mapView.addPOIItem(marker);
+
+
         searchText = view.findViewById(R.id.search_txt);
         searchText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,7 +64,6 @@ public class MapActivity extends Fragment {
                 startActivity(intent);
             }
         });
-
 
         return view;
     }
