f680cee819898869d57d4ff94ce164297ede1c82
Eugene Kim <ekim9725@naver.com>
2021-10-07 16:55:32 +0900

    Edit GalleryActivity and related layouts (show images by query)

--- app/src/main/java/com/example/wookie/Gallery/GalleryActivity.java
+++ app/src/main/java/com/example/wookie/Gallery/GalleryActivity.java
@@ -2,28 +2,123 @@ package com.example.wookie.Gallery;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.wookie.Feed.PostAdapter;
+import com.example.wookie.Models.Post;
 import com.example.wookie.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 public class GalleryActivity extends Fragment {
+
+    private String TAG = "GalleryActivity";
+    private View view;
+    private TextView groupNameTxt;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Post> postList;
+
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
+
     public GalleryActivity(){
         // 비어있는 constructor 필요
     }
-
-    private View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.activity_gallery, container, false);
 
+        groupNameTxt = view.findViewById(R.id.group_name);
+        recyclerView = view.findViewById(R.id.recyclerView);
+
+        recyclerView.setHasFixedSize(true);
+//        recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL)); // 구분선 추가
+        layoutManager = new GridLayoutManager(this.getContext(), 3);
+//        ((GridLayoutManager) layoutManager).setReverseLayout(true);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        final String groupId = getActivity().getIntent().getStringExtra("groupId");
+
+        setGroupName(groupId); // 방제 설정
+        setGallery(groupId);
+
         return view;
     }
+
+    // 갤러리 이미지 목록 설정
+    private void setGallery(String groupId) {
+        postList = new ArrayList<>(); // Post 객체를 담을 Array리스트
+
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("Post").child(groupId);
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                        Post post = snapshot.getValue(Post.class);
+                        if(post.isGallery() == true){
+                            postList.add(post);
+                        }
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e(TAG, error.toString());
+            }
+        });
+
+        adapter = new GalleryAdapter(postList, this.getContext());
+        recyclerView.setAdapter(adapter);
+    }
+
+    // 방제 설정
+    private void setGroupName(String groupId) {
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("group");
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    String groupName = dataSnapshot.child(groupId).child("groupName").getValue().toString();
+                    groupNameTxt.setText("갤러리(" + groupName + ")");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e(TAG, error.toString());
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/com/example/wookie/Gallery/GalleryAdapter.java
@@ -0,0 +1,111 @@
+package com.example.wookie.Gallery;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.example.wookie.Models.Post;
+import com.example.wookie.Post.ReadPostActivity;
+import com.example.wookie.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.PostViewHolder> {
+    private String TAG = "GalleryAdapter";
+    private ArrayList<Post> postList;
+    private Context context;
+
+    private FirebaseDatabase database;
+    private Query query;
+
+    public GalleryAdapter(ArrayList<Post> postList, Context context){
+        this.postList = postList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_gallery, parent, false);
+        PostViewHolder holder = new PostViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
+
+        // 작성자 설정
+        String userId = postList.get(position).getUserId();
+        database = FirebaseDatabase.getInstance();
+        query = database.getReference("users").orderByChild("userId").equalTo(userId);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String name = snapshot.child(userId).child("userName").getValue().toString();
+
+                holder.postUserNameTxt.setText(name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e(TAG, error.toString());
+            }
+        });
+
+        // 작성일 설정
+        holder.postDateTxt.setText(postList.get(position).getPostDate());
+        // 이미지 설정
+        Glide.with(context).load(postList.get(position).getPostImg())
+                .transform(new CenterCrop(), new RoundedCorners(1)).into(holder.galleryImage);
+    }
+
+    @Override
+    public int getItemCount() { return (postList != null ? postList.size() : 0); }
+
+
+    public class PostViewHolder extends RecyclerView.ViewHolder {
+        ImageView galleryImage;
+        TextView postUserNameTxt, postDateTxt;
+
+        public PostViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            this.galleryImage = itemView.findViewById(R.id.gallery_image);
+            this.postUserNameTxt = itemView.findViewById(R.id.post_user_name_txt);
+            this.postDateTxt = itemView.findViewById(R.id.post_date_txt);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String groupId = postList.get(getLayoutPosition()).getGroupId();
+                    String postId = postList.get(getLayoutPosition()).getPostId();
+
+                    Intent intent = new Intent(itemView.getContext(), ReadPostActivity.class);
+                    intent.putExtra("groupId", groupId);
+                    intent.putExtra("postId", postId);
+                    itemView.getContext().startActivity(intent);
+                    //TODO: readpostactivity에서 이전 액티비티에 따라 다른 화면 뜨게 만들기
+//                    ((BottomNaviActivity)itemView.getContext()).finish();
+                }
+            });
+        }
+    }
+}
